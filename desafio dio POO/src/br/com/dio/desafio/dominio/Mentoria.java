d27d0bf802f5981e9ed2e7fbdeb2c68d0e12e527
Leandro Santos <santosleandro1805@gmail.com>
2025-01-22 14:26:04 -0300

    Arrumando Bug que possa dar erro

--- desafio dio POO/src/br/com/dio/desafio/dominio/Mentoria.java
+++ desafio dio POO/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -7,7 +7,7 @@ public class Mentoria extends Conteudo{
     private LocalDate data;
 
     @Override
-    public double calcularxp() {
+    public double calcularXp() {
         return XP_PADRAO + 20d;
     }
 
